73008196141b556f969734707c14ae1f8eaf0c5a
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-02-23 17:26:18 +0200

    fixsed tabs

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,10 @@ import './import/select.js';
 import './import/swiper.js';
 import './import/tabs.js';
 
+// Якорь
+$('.intro__btn').on('click','a', function(event) {
+  event.preventDefault();
+  var id  = $(this).attr('href'),
+    top = $(id).offset().top;
+  $('body,html').animate({scrollTop: top}, 1000);
+});
