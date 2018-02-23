362b307993ebb59a3ca560ccafe333b491d58155
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-02-23 23:07:57 +0200

    fixsed select

--- src/js/app.js
+++ src/js/app.js
@@ -9,3 +9,31 @@ $('.intro__btn').on('click','a', function(event) {
     top = $(id).offset().top;
   $('body,html').animate({scrollTop: top}, 1000);
 });
+// блок локации
+// $('.header__block__location').on('click', function() {
+//   $('.header__block__location__menu').addClass('active');
+// });
+// $('').on('click', function() {
+//   if()
+//   $('.header__block__location__menu').removeClass('active');
+// });
+$('.header__block__location').click(function() {
+  // $(this).toggleClass("active");
+  if($(this).hasClass('active')) {
+    $(this).removeClass('active');
+    $(this).find($('a')).slideUp();
+  } else
+    active($(this));
+});
+
+function active(crr) {
+  $('.header__block__location').removeClass('active');
+  $('.header__block__location__menu').slideUp();
+  crr.addClass('active');
+  crr.find($('a')).slideDown();
+}
+$(document).on('click', function() {
+  if ($(event.target).closest('.header__block__location').length === 0) {
+    $('.header__block__location').removeClass('active');
+  }
+});
