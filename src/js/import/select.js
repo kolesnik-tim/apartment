5c01f6318a4a110a10a1b6d7fabc2cb3f81ccbf6
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-02-25 20:00:33 +0200

    fixsed select

--- src/js/import/select.js
+++ src/js/import/select.js
@@ -1,16 +1,13 @@
 import Select from 'tether-select';
 
-// select header
-$('select.select-first').each(function() {
-  var selectInstance = new Select({
-    el: this,
-    className: 'select-theme-location',
-  });
-});
 //select intro
-$('select.select-last').each(function() {
-  var selectInstance = new Select({
-    el: this,
-    className: 'select-theme-default',
-  });
+$(window).on('load resize', function() {
+  if ($(window).width() >= '767') {
+    $('select.select-last').each(function() {
+      var selectInstance = new Select({
+        el: this,
+        className: 'select-theme-default',
+      });
+    });
+  }
 });
